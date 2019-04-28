c8be55e281fa62815ac268cd226504a47fd8d184
duperez <eduardobianconiperez@gmail.com>
2019-04-28 19:03:30 -0300

    correcao de padrao de codigo

--- masp.c
+++ masp.c
@@ -1,61 +1,58 @@
 void criaMasp(){
-  glColor3f(1.0, 0.0, 0.0);
   /* suporte esquerda 1*/ 
   glPushMatrix();
-  glTranslatef (-23.5, 10.5, 18.5);
-  glScalef (3.0, 20.0, 3.0);
-  glutSolidCube (1.0);
+    glColor3f(1.0, 0.0, 0.0);
+    glTranslatef (-23.5, 10.5, 18.5);
+    glScalef (3.0, 20.0, 3.0);
+    glutSolidCube (1.0);
   glPopMatrix();
 
-
-  glColor3f(1.0, 0.0, 0.0);
     /* suporte direita 1*/ 
   glPushMatrix();
-  glTranslatef (23.5, 10.5, 18.5);
-  glScalef (3.0, 20.0, 3.0);
-  glutSolidCube (1.0);
+    glColor3f(1.0, 0.0, 0.0);
+    glTranslatef (23.5, 10.5, 18.5);
+    glScalef (3.0, 20.0, 3.0);
+    glutSolidCube (1.0);
   glPopMatrix();
 
-
-  glColor3f(1.0, 0.0, 0.0);
     /* suporte esquerda 2*/ 
   glPushMatrix();
-  glTranslatef (-23.5, 10.5, 0);
-  glScalef (3.0, 20.0, 3.0);
-  glutSolidCube (1.0);
+    glColor3f(1.0, 0.0, 0.0);
+    glTranslatef (-23.5, 10.5, 0);
+    glScalef (3.0, 20.0, 3.0);
+    glutSolidCube (1.0);
   glPopMatrix();
 
-
-  glColor3f(1.0, 0.0, 0.0);
     /* suporte direita 2*/ 
   glPushMatrix();
-  glTranslatef (23.5, 10.5, 0);
-  glScalef (3.0, 20.0, 3.0);
-  glutSolidCube (1.0);
+    glColor3f(1.0, 0.0, 0.0);
+    glTranslatef (23.5, 10.5, 0);
+    glScalef (3.0, 20.0, 3.0);
+    glutSolidCube (1.0);
   glPopMatrix();
 
       /* corpo*/ 
   glPushMatrix();
-  glColor3f(0.44, 0.5, 0.56);
-  glTranslatef (0, 10.5, 9);
-  glScalef (44.0, 10.0, 30);
-  glutSolidCube (1.0);
+    glColor3f(0.44, 0.5, 0.56);
+    glTranslatef (0, 10.5, 9);
+    glScalef (44.0, 10.0, 30);
+    glutSolidCube (1.0);
   glPopMatrix();
 
-    glColor3f(1.0, 0.0, 0.0);
       /* superior esquerdo*/ 
   glPushMatrix();
-  glTranslatef (0, 18, 0);
-  glScalef (44.0, 5.0, 3);
-  glutSolidCube (1.0);
+    glColor3f(1.0, 0.0, 0.0);
+    glTranslatef (0, 18, 0);
+    glScalef (44.0, 5.0, 3);
+    glutSolidCube (1.0);
   glPopMatrix();
 
-    glColor3f(1.0, 0.0, 0.0);
       /* superior direito*/ 
   glPushMatrix();
-  glTranslatef (0, 18, 18.5);
-  glScalef (44.0, 5.0, 3);
-  glutSolidCube (1.0);
+    glColor3f(1.0, 0.0, 0.0);
+    glTranslatef (0, 18, 18.5);
+    glScalef (44.0, 5.0, 3);
+    glutSolidCube (1.0);
   glPopMatrix();
 
         /* rampa trazeira */
